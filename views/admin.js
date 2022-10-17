15a585a307c2212aa46c002211605a4fd722a213
Isabela <r_isabela@outlook.com>
2022-10-17 16:25:07 +0100

    adicionando views

---
+++ views/admin.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css"></link>
+</head>
+<body>
+    <div class="container">
+
+        <form class="form admin" id="formAcesso">
+            <div>
+                <label>Usuário</label>
+                <input type="text" id="usuario">
+            </div>
+            <div>
+                <label>Senha</label>
+                <input type="password" id="password">
+            </div>
+            <div>
+                <button>Entrar</button>
+            </div>
+        </form>
+
+    </div>
+
+    <script src="js/main.js"></script>
+</body>
+</html>
